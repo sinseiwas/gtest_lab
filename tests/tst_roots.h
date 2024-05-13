c47e13622cb2c48c300fb52b3474bc3119c386cb
danil <d-suahnuv@mail.ru>
2024-05-13 19:44:54 +0300

    tst_roots

--- tests/tst_roots.h
+++ tests/tst_roots.h
@@ -11,33 +11,39 @@ extern "C" {
 #include "roots.h"
 }
 
+int result;
 double arr[2];
 
 TEST(roots, right_answer) {
-    ASSERT_EQ(roots(arr, 1, -5, 6), 1);
+    result = roots(arr, 1, -5, 6);
+    ASSERT_EQ(result, 1);
     ASSERT_EQ(arr[0], 3);
     ASSERT_EQ(arr[1], 2);
 }
 
 TEST(roots, float_input_float_out) {
-    ASSERT_EQ(roots(arr, 1, -4, 3.75), 1);
+    result = roots(arr, 1, -4, 3.75);
+    ASSERT_EQ(result, 1);
     ASSERT_EQ(arr[0], 2.5);
     ASSERT_EQ(arr[1], 1.5);
 }
 
 TEST(roots, one_root) {
-    ASSERT_EQ(roots(arr, 1, -6, 9), 1);
+    result = roots(arr, 1, -6, 9);
+    ASSERT_EQ(result, 1);
     ASSERT_EQ(arr[0], 3);
     ASSERT_EQ(arr[1], 3);
 }
 
 TEST(roots, invalid_input) {
-    ASSERT_EQ(roots(arr, 0, 0, 0), 0);
+    result = roots(arr, 0, 0, 0);
+    ASSERT_EQ(result, 0);
 }
 
 
 TEST(roots, no_root) {
-    ASSERT_EQ(roots(arr, 1, 2, 3), 0);
+    result = roots(arr, 1, 2, 3);
+    ASSERT_EQ(result, 0);
 }
 
 #endif // TST_ROOTS_H
